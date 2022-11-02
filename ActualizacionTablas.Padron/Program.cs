9162ceee4fb641ccffa11fe4c9dba6758a46d972
manupaw <manupaw2000@gmail.com>
2022-11-02 12:05:51 -0300

    logica para leer archivo

--- ActualizacionTablas.Padron/Program.cs
+++ ActualizacionTablas.Padron/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,37 @@ namespace ActualizacionTablas.Padron
 {
     class Program
     {
+        const string PATH = @"C:\Users\79\Desktop\Debitia Report 10_28_2022.csv";
+
         static void Main(string[] args)
         {
+
+            //Conectar a bd
+
+            //Crear tablas
+
+            //Leer archivo y subirlo
+            using (FileStream fs = File.Open(PATH, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (BufferedStream bs = new BufferedStream(fs))
+            using (StreamReader sr = new StreamReader(bs))
+            {
+                int i = 0;
+                string line;
+                while ((line = sr.ReadLine()) != null && i < 50)
+                {
+
+                    Console.WriteLine(line);
+
+                    i++;
+                }
+            }
+
+            //Crear indices
+
+            //Cambiar nombre de nueva tabla
+
+            Console.Read();
+
         }
     }
 }
